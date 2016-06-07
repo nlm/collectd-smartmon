a442a036701330df1f65d728e167b3e11e26b4de
Nicolas Limage <github@xephon.org>
2016-06-07 17:41:11 +0200

    added autodetection, runtime devices check, new options, bugfixes

--- collectd-smartmon.py
+++ collectd-smartmon.py
@@ -32,35 +32,67 @@ class SmartDevice(object):
 
 
 def check_dev(dev):
-    if not os.path.exists('/dev/{0}'.format(dev)):
-        raise argparse.ArgumentTypeError('/dev/{0} does not exists'.format(dev))
-    return dev
+    return os.path.exists('/dev/{0}'.format(dev))
 
+def get_filelist(dirname, pattern):
+    return set([f for f in os.listdir(dirname) if re.match(pattern, f)])
+
+def expand_devices(devlist):
+    expanded_devlist = []
+    for dev in devlist:
+        if dev == 'autodetect':
+            expanded_devlist.extend(get_filelist('/dev', r'^sd[a-z]+$'))
+        else:
+            expanded_devlist.append(dev)
+    return sorted(list(set(expanded_devlist)))
+
+def smartmon_loop(devices, hostname, interval):
+    while True:
+        for dev in devices:
+            if check_dev(dev):
+                for attr in SmartDevice(dev).attributes():
+                    print('PUTVAL "{hostname}/smart-{dev}'
+                          '/absolute-{id}_{attr}"'
+                          ' interval={interval} N:{value}'
+                          .format(hostname=hostname, dev=dev, id=attr['id'],
+                                  attr=attr.get('name'), interval=interval,
+                                  value=attr['raw_value']))
+        time.sleep(interval)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('DEV', nargs='+',
-                        help='devices to check', type=check_dev)
+    parser.add_argument('dev', nargs='*',
+                        help='devices to check (default: autodetect)')
     parser.add_argument('-H', '--hostname', type=str,
                         help='override hostname provided by collectd',
                         default=os.environ.get('COLLECTD_HOSTNAME'))
     parser.add_argument('-i', '--interval', type=int,
                         help='override interval provided by collectd',
                         default=int(float(os.environ.get('COLLECTD_INTERVAL', 300))))
+    parser.add_argument('-c', '--dont-check-devs',
+                        action='store_true', default=False,
+                        help='do not check devices existence at startup')
     args = parser.parse_args()
 
-    hostname = args.hostname or subprocess.check_output(['hostname', '-f']).strip()
+    hostname = (args.hostname
+                or subprocess.check_output(['hostname', '-f']).strip())
+    if len(hostname) == 0:
+        parser.error('unable to detect hostname')
     interval = max(args.interval, 5)
-    while True:
-        for dev in args.DEV:
-            for attr in SmartDevice('sda').attributes():
-                print('PUTVAL "{hostname}/smart-{dev}/absolute-{id}_{attr}"'
-                      ' interval={interval} N:{value}'
-                      .format(hostname=hostname, dev=dev, id=attr['id'],
-                              attr=attr.get('name'), interval=interval,
-                              value=attr['raw_value']))
-        time.sleep(interval)
+    if len(args.dev) == 0:
+        devices = expand_devices(['autodetect'])
+    else:
+        devices = expand_devices(args.dev)
 
+    if not args.dont_check_devs:
+        for dev in devices:
+            if not check_dev(dev):
+                parser.error('device "/dev/{0}" does not exist'.format(dev))
+
+    try:
+        smartmon_loop(devices, hostname, interval)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
